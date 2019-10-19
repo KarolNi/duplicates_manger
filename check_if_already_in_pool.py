d4c84ddb2dd85fccea4c78c47a8efee0578c51fe
KarolNi <git@karolni.pl>
2019-10-19 23:34:16 +0200

    added tool for checking if files in one list are already in second list

---
+++ check_if_already_in_pool.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""pyscript.py
+A simple python script template.
+http://ajminich.com/2013/08/01/10-things-i-wish-every-python-script-did/
+"""
+
+import argparse
+
+import json
+import os
+import sys
+
+
+
+def check_if_already_in_pool(list_of_candidates, list_of_pool):
+    list_of_candidates = sorted(list_of_candidates, key=lambda file: file['sha512'])
+    list_of_pool = sorted(list_of_pool, key=lambda file: file['sha512'])
+    i = 0
+    for candidate in list_of_candidates:
+        if i >= len(list_of_pool):
+            break
+        if candidate['sha512'] == list_of_pool[i]['sha512']:  # same file
+            continue
+        if candidate['sha512'] < list_of_pool[i]['sha512']:  # file is not present in pool
+            print(candidate)
+            continue
+        while candidate['sha512'] > list_of_pool[i]['sha512']:  # other file in pool
+            i = i+1
+            if i >= len(list_of_pool):
+                break
+
+def main():
+    candidates_file = 'test_candidates.json'
+    list_of_candidates = json.load(open(candidates_file))
+
+    pool_file = 'test_pool.json'
+    list_of_pool = json.load(open(pool_file))
+
+
+    check_if_already_in_pool(list_of_candidates, list_of_pool)
+
+
+
+if __name__ == '__main__':
+    main()
