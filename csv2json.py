8dd129afdbe0cd5ec49d669ba84d406b7e028a89
KarolNi <git@karolni.pl>
2019-10-19 23:30:15 +0200

    added csv2json converter

---
+++ csv2json.py
@@ -0,0 +1,17 @@
+import csv
+import json
+import sys
+
+output_file = sys.argv.pop(-1) + '.json'
+sys.argv.pop(0)
+
+data = list()
+
+for input_file in sys.argv:
+  with open(input_file) as file:
+    data = data + (list(csv.DictReader(file)))
+
+f = open(output_file, 'w')
+f.write(json.dumps(data))
+f.close
+
