a20a7a9a3b3ec9a53d7b953fb0134767e176b42e
KarolNi <git@karolni.pl>
2019-11-10 23:10:22 +0100

    added results file from argv in check_if_already_in_pool, fixed (TBD) algorithm error

--- check_if_already_in_pool.py
+++ check_if_already_in_pool.py
@@ -4,7 +4,7 @@
 A simple python script template.
 http://ajminich.com/2013/08/01/10-things-i-wish-every-python-script-did/
 
-usage: check_if_already_in_pool.py pool.json candidates.json
+usage: check_if_already_in_pool.py pool.json candidates.json result.json
 """
 
 # import argparse
@@ -19,18 +19,21 @@ def check_if_already_in_pool(list_of_candidates, list_of_pool):
     list_of_candidates = sorted(list_of_candidates, key=lambda file: file['sha512'])
     list_of_pool = sorted(list_of_pool, key=lambda file: file['sha512'])
     i = 0
+    ret = list()
     for candidate in list_of_candidates:
+        while candidate['sha512'] > list_of_pool[i]['sha512']:  # other file in pool
+            i = i+1
+            if i >= len(list_of_pool):
+                break
         if i >= len(list_of_pool):
             break
         if candidate['sha512'] == list_of_pool[i]['sha512']:  # same file
             continue
         if candidate['sha512'] < list_of_pool[i]['sha512']:  # file is not present in pool
-            print(candidate)
+            ret.append(candidate)
             continue
-        while candidate['sha512'] > list_of_pool[i]['sha512']:  # other file in pool
-            i = i+1
-            if i >= len(list_of_pool):
-                break
+
+    return ret
 
 def main():
     candidates_file = sys.argv[2]
@@ -40,8 +43,11 @@ def main():
     list_of_pool = json.load(open(pool_file))
 
 
-    check_if_already_in_pool(list_of_candidates, list_of_pool)
+    data = check_if_already_in_pool(list_of_candidates, list_of_pool)
 
+    f = open(sys.argv[3], 'w')
+    f.write(json.dumps(data))
+    f.close()
 
 
 if __name__ == '__main__':
